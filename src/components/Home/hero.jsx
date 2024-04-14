a1e0e9044ffb5aa041190f6d4141cf987d9d2939
Amit Mahto <amitextinct@gmail.com>
2024-04-14 15:44:52 +0530

    blocked mobile access

--- src/components/Home/hero.jsx
+++ src/components/Home/hero.jsx
@@ -8,29 +8,8 @@ import {
   IconButton,
   Typography,
 } from "@material-tailwind/react";
-import {
-  RectangleStackIcon,
-  UserCircleIcon,
-  CommandLineIcon,
-  Squares2X2Icon,
-} from "@heroicons/react/24/solid";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-function NavItem({ children }) {
-  return (
-    <li>
-      <Typography
-        as="a"
-        href="#"
-        variant="paragraph"
-        color="blue-gray"
-        className="text-blue-gray-700 flex items-center gap-2 font-medium"
-      >
-        {children}
-      </Typography>
-    </li>
-  );
-}
 
 function HeroSection16() {
   const [open, setOpen] = React.useState(false);
@@ -74,30 +53,13 @@ function HeroSection16() {
         </div>
         <Collapse open={open}>
           <div className="container mx-auto mt-3 border-t border-blue-gray-50 px-2 pt-4">
-            <ul className="flex flex-col gap-4">
-              <NavItem>
-                <RectangleStackIcon className="h-5 w-5" />
-                Pages
-              </NavItem>
-              <NavItem>
-                <UserCircleIcon className="h-5 w-5" />
-                Account
-              </NavItem>
-              <NavItem>
-                <Squares2X2Icon className="h-5 w-5" />
-                Blocks
-              </NavItem>
-              <NavItem>
-                <CommandLineIcon className="h-5 w-5" />
-                Docs
-              </NavItem>
-            </ul>
+            
             <div className="mt-6 mb-4 flex items-center gap-4">
               <Link to="/login">
                 <Button variant="text">Log in</Button>
               </Link>
               <Link to="/signup">
-                <Button color="gray">buy now</Button>
+                <Button color="gray">Sign up</Button>
               </Link>
             </div>
           </div>
